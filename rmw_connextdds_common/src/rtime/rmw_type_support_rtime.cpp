0def394bb909a5b0b65c379600b28bcd0d7fd7e9
Andrea Sorbini <asorbini@rti.com>
2021-03-05 02:30:14 -0800

    Linter error

--- rmw_connextdds_common/src/rtime/rmw_type_support_rtime.cpp
+++ rmw_connextdds_common/src/rtime/rmw_type_support_rtime.cpp
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <string>
-
 #include <string.h>
+
+#include <string>
 
 #include "rmw_connextdds/type_support.hpp"
 #include "rmw_connextdds/rmw_impl.hpp"

6d929153b72fa626f920228e91a45a24b8d72394
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2021-03-15 12:10:36 -0300

    Delete debug log
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rmw_connextdds_common/src/common/rmw_context.cpp
+++ rmw_connextdds_common/src/common/rmw_context.cpp
@@ -209,7 +209,6 @@ rmw_context_impl_t::initialize_participant(const bool localhost_only)
     this->endpoint_qos_override_policy =
       rmw_context_impl_t::endpoint_qos_override_policy_t::DDSTopics;
     try {
-      fprintf(stderr, "regex found: %s", &endpoint_qos_policy[sizeof(dds_topic_policy_prefix)]);
       this->endpoint_qos_override_policy_topics_regex = &endpoint_qos_policy[sizeof(dds_topic_policy_prefix) - 1u];
     } catch (std::regex_error & err) {
       RMW_CONNEXT_LOG_ERROR_A_SET(

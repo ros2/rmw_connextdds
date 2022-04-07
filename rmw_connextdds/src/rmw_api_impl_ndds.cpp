3fc9c4a4fb781f902484ef7ca5d08d4b4450bdce
Chris Lalancette <clalancette@openrobotics.org>
2022-04-07 16:44:42 -0400

    Add missing semicolon

--- rmw_connextdds/src/rmw_api_impl_ndds.cpp
+++ rmw_connextdds/src/rmw_api_impl_ndds.cpp
@@ -966,7 +966,7 @@ rmw_feature_supported(rmw_feature_t feature)
     case RMW_FEATURE_MESSAGE_INFO_RECEPTION_SEQUENCE_NUMBER:
     case RMW_FEATURE_MESSAGE_INFO_PUBLICATION_SEQUENCE_NUMBER:
       {
-        return true
+        return true;
       }
     default:
       {

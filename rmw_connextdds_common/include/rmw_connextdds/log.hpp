4d506179307a49e2debe9a96ad03692536c53a8b
Francisco Gallego Salido <fgallego@rti.com>
2025-03-07 13:07:42 +0100

    Don't print debug messages in default logging mode

--- rmw_connextdds_common/include/rmw_connextdds/log.hpp
+++ rmw_connextdds_common/include/rmw_connextdds/log.hpp
@@ -137,16 +137,9 @@
     (fmt_), \
     __VA_ARGS__);
 
-#define RMW_CONNEXT_LOG_DEBUG(msg_) \
-  RCUTILS_LOG_DEBUG_NAMED( \
-    RMW_CONNEXTDDS_ID, \
-    (msg_));
+#define RMW_CONNEXT_LOG_DEBUG(msg_)
 
-#define RMW_CONNEXT_LOG_DEBUG_A(fmt_, ...) \
-  RCUTILS_LOG_DEBUG_NAMED( \
-    RMW_CONNEXTDDS_ID, \
-    (fmt_), \
-    __VA_ARGS__);
+#define RMW_CONNEXT_LOG_DEBUG_A(fmt_, ...)
 
 #define RMW_CONNEXT_LOG_TRACE(msg_)
 

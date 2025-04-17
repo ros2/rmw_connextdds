87e28020fbf4d541054fc35a7684a128d0383592
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2025-04-17 10:12:15 -0500

    Fix cpplint regression
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- rmw_connextdds_common/src/common/rmw_type_support.cpp
+++ rmw_connextdds_common/src/common/rmw_type_support.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <exception>
 #include <string.h>
+#include <exception>
 #include <string>
 
 #include "rcpputils/scope_exit.hpp"

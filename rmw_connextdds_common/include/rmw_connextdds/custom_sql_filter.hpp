d4d5f2192ab70edda51dc74f06508d1f15575758
Francisco Gallego Salido <fgallego@rti.com>
2025-12-02 16:57:12 +0100

    Use DDS_DomainParticipant_register_contentfilterI instead of DDS_ContentFilter_register_filter

--- rmw_connextdds_common/include/rmw_connextdds/custom_sql_filter.hpp
+++ rmw_connextdds_common/include/rmw_connextdds/custom_sql_filter.hpp
@@ -49,11 +49,10 @@ extern "C" {
 // be registered as "built-in". We need this because we want this custom filter
 // to be a replacement for the built-in SQL-like filter.
 RMW_CONNEXTDDS_PUBLIC
-DDS_ReturnCode_t
-DDS_ContentFilter_register_filter(
-  DDS_DomainParticipant * participant,
-  const char * name,
-  const struct DDS_ContentFilter * filter,
+DDS_ReturnCode_t DDS_DomainParticipant_register_contentfilterI(
+  DDS_DomainParticipant *participant,
+  const char *name,
+  const struct DDS_ContentFilter *filter,
   const DDS_ContentFilterEvaluateFunction evaluateOnSerialized,
   const DDS_ContentFilterWriterEvaluateFunction writerEvaluateOnSerialized,
   const DDS_ContentFilterQueryFunction query,
